fe7893754d91bc2db3611db5dc2763eea66810c6
titospek <tito.spek@gmail.com>
2021-09-07 10:26:08 +0200

    Initial commit

---
+++ phpinfo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <title>php-bestand</title>
+</head>
+<body>
+<?php
+    echo phpinfo();
+    echo $_SERVER["HTTP_USER_AGENT"];
+?>
+</body>
+</html>
